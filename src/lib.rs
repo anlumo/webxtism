208c1af01d21d0ac1074554ecbd8888bf13c2a50
Andreas Monitzer <andreas@monitzer.com>
2024-11-03 20:44:03 +0100

    Initial commit. Created makefile task to compile the extism kernel.

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+pub use extism_convert;
+pub use extism_manifest;
+pub use wasmer;
+
+pub const EXTISM_ENV_MODULE: &str = "extism:host/env";
+pub const EXTISM_USER_MODULE: &str = "extism:host/user";
