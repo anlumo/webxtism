10ee0027788e7925e6f0881d97e74769f5c8cfff
Andreas Monitzer <andreas@monitzer.com>
2024-11-05 18:14:01 +0100

    Allow setting compile options and the trap handler for the wasmer runtime.

--- src/context.rs
+++ src/context.rs
@@ -1,6 +1,7 @@
 use std::sync::RwLock;
 
-use wasmer::{Module, Store};
+use typed_builder::TypedBuilder;
+use wasmer::{Module, NativeEngineExt, Store, TrapHandlerFn, Tunables};
 
 const RUNTIME: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/extism-runtime.wasm"));
 
@@ -14,19 +15,38 @@ unsafe impl Sync for Context {}
 
 impl Default for Context {
     fn default() -> Self {
-        // TODO: allow for configuration on non-web targets
-        let store = Store::default();
-        let runtime = Module::new(&store, RUNTIME).unwrap();
-
-        Self {
-            store: RwLock::new(store),
-            runtime,
-        }
+        ContextBuilder::builder().build()
     }
 }
 
 impl Context {
     pub fn store(&self) -> std::sync::RwLockWriteGuard<'_, wasmer::Store> {
         self.store.write().unwrap()
+    }
+}
+
+#[derive(TypedBuilder)]
+#[builder(build_method(into = Context))]
+pub struct ContextBuilder {
+    #[builder(default, setter(strip_option))]
+    trap_handler: Option<Box<TrapHandlerFn<'static>>>,
+    #[builder(default, setter(strip_option))]
+    tunables: Option<Box<dyn Tunables + Send + Sync + 'static>>,
+}
+
+impl From<ContextBuilder> for Context {
+    fn from(builder: ContextBuilder) -> Self {
+        let mut store = Store::default();
+        store.set_trap_handler(builder.trap_handler);
+        if let Some(tunables) = builder.tunables {
+            store.engine_mut().set_tunables(tunables);
+        }
+
+        let runtime = Module::new(&store, RUNTIME).unwrap();
+
+        Self {
+            store: RwLock::new(store),
+            runtime,
+        }
     }
 }
